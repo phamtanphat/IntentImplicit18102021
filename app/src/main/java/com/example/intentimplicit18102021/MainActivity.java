a977173f00cc5d4529e9a18f42e83b7ba5faa93e
phamtanphat <phatdroid94@gmail.com>
2021-12-24 18:18:33 +0700

    request permission camera

--- app/src/main/java/com/example/intentimplicit18102021/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit18102021/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.intentimplicit18102021;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultRegistry;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -16,6 +25,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,13 +46,40 @@ public class MainActivity extends AppCompatActivity {
         mBtnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(
-                            MainActivity.this,
-                            new String[]{Manifest.permission.CAMERA},
-                            REQUEST_CODE_CAMERA
-                    );
-                } else if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                requestLauncherCamera.launch(Manifest.permission.CAMERA);
+            }
+        });
+    }
+
+    private ActivityResultLauncher<String> requestLauncherCamera = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+        @Override
+        public void onActivityResult(Boolean result) {
+            if (result) {
+                Log.d("BBB", "Allow");
+            } else {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Camera permission go to setting enable");
+                    builder.setPositiveButton("Setting", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            Uri uri = Uri.fromParts(
+                                    "package",
+                                    BuildConfig.APPLICATION_ID, null
+                            );
+                            intent.setData(uri);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            requestLauncherCamera.launch(Manifest.permission.CAMERA);
+                        }
+                    }).show();
+                } else {
                     Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
                     Uri uri = Uri.fromParts(
                             "package",
@@ -53,17 +90,7 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
             }
-        });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CODE_CAMERA){
-            if (grantResults[0] != PackageManager.PERMISSION_GRANTED){
-
-            }
         }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    });
 
-    }
 }
